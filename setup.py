5e46bee8f098edd72d85121eabd07970808aa8cc
Ronald Seoh <ronaldseoh@icloud.com>
2019-12-05 20:09:45 -0500

    setup.py: typo in description

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup, find_packages
 
 setup(name='ronald_bdl',
       version='0.1',
-      description='An experimental Python package for learning Bayesian Neural Network..',
+      description=(
+            'An experimental Python package '
+            'for learning Bayesian Neural Network.'),
       url='https://github.com/ronaldseoh/ronald_bdl',
       author='Ronald Seoh',
       author_email='ronaldseoh@icloud.com',
